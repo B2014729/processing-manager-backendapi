aca239e89e406f41d7517fad46884cac01687b13
B2014729 <bangduong870@gmail.com>
2023-09-13 23:27:24 +0700

    Cap nhat cac route ve nhan vien

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,14 +2,33 @@ import express from 'express';
 
 import { getBlockChain } from '../controllers/processController.js';
 
+import * as staffController from '../controllers/staffController.js';
+
 let router = express.Router();
 
 const initWebRoute = (app) => {
-    router.get('/', (req, res, next) => {
-        next();
-    }, getBlockChain);
+    //Acount
 
-    return app.use('/', router);
+    //Staff
+    router.route('/create-user')
+        .post(staffController.createStaff)
+    router.route('/user/:id')
+        .get(staffController.getStaffInfor)
+        .put(staffController.updateStaffInfor)
+        .delete(staffController.deleteStaff)
+
+    router.route('/staff-list')
+        .get(staffController.getStaffList)
+
+    // router.route('/edit-user')
+    //     .post(staffController.updateStaffInfor)
+
+    //Processing
+    router.route('/myblockchain')
+        .get(getBlockChain)
+
+    //Product
+    return app.use('/api', router);
 }
 
 export default initWebRoute;
